62a2a65a3b282e9046f7c79433ff688a4ffc219e
Connor Worrell <chemicalcoal1@gmail.com>
2022-05-27 22:47:38 -0600

    !! functionality for sudo and cdf sourcing

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,15 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+function sudo
+	if test "$argv" = !!
+		eval command sudo $history[1]
+	else
+		command sudo $argv
+	end
+end
+
+function cdf
+	source ~/.scripts/cdf $argv
+end
